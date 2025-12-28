7f70fa05840fd675c0875513c3bdb03a87bed19b
Haaniya-ismail186 <haaniyaismail186@gmail.com>
2025-12-29 03:52:59 +0500

    First commit

---
+++ chapter 4/app.js
@@ -0,0 +1,27 @@
+//Declare 3 variables in one statement//
+var firstName, last$Name, _age;
+
+//Declare 5 legal names//
+var userName, _visit, my_Name, $price, first_name;
+
+//Display in browser//
+
+var rules = "<h1>Rules for naming JS variables:</h1>";
+console.log(rules);
+document.write(rules);
+
+var varNames = "Variable names can only contain numbers, $, and _. For example: $my_1stVariable<br>";
+console.log(varNames);
+document.write(varNames);
+
+var varLetters = "Variables must begin with a letter, $ or _ . For example: $name, _name or name<br>";
+console.log(varLetters);
+document.write(varLetters);
+
+var varcase = "Variable names are case sensitive<br>";
+console.log(varcase);
+document.write(varcase);
+
+var keyWords = "Variable names should not be JS keywords";
+console.log(keyWords);
+document.write(keyWords);
